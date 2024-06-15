b8de776c85824b3b7e1e992b6cb2583d8e087048
Andre Gregori Sangari <andresangari12@gmail.com>
2024-06-16 01:18:50 +0700

    first commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import uvicorn
 
 app = FastAPI()
 
@@ -106,5 +108,4 @@ async def get_recommendations(user_id: int):
     }
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
